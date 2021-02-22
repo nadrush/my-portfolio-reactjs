ded06d31a5a10a9a18ade7a32dd3a78962de79a2
Nadeem Abdur Rasheed <nadimrasheed0@gmail.com>
2021-02-22 11:24:20 +0700

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import Footer from './components/Footer';
 import './App.css';
+import BgImage from './assets/images/bg.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <>
+      {/* <div className="bg"></div> */}
+      <div style={{ backgroundImage: `url(${BgImage})` }}>test</div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <a href='https://www.freepik.com/photos/background'>Background photo created by benzoix - www.freepik.com</a>
+        </footer>
+    )
+}
+
+export default Footer
